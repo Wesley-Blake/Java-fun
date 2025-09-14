c61eeced27b5164a50c2d53e9d4f740bf5cf3a58
Wesley Blake <wesleyblake95@gmail.com>
2025-09-14 10:21:55 -0700

    Added basic sort cli.

---
+++ sort/sort.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class sort {
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			System.err.println("To many args.");
+			System.exit(1);
+		}
+
+		Path path = Paths.get(args[0]);
+		if (Files.notExists(path)) {
+			System.err.println(
+					"File: " +
+					path +
+					"doesn't exist.");
+			System.exit(1);
+		}
+
+		ArrayList<String> readLines = new ArrayList<>();
+		try (BufferedReader reader = Files.newBufferedReader(path)) {
+			String line;
+			while((line = reader.readLine()) != null) {
+				readLines.add(line);
+			}
+			if (!readLines.isEmpty()) {
+				Collections.sort(readLines);
+				for (String i : readLines) {
+					System.out.println(i);
+				}
+			} else {
+				System.out.println("Fuck");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
